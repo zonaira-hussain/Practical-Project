13f7783abdd19392fd0d2dff706975e79f1b8e7c
Ubuntu <ubuntu@ip-172-31-29-202.eu-west-1.compute.internal>
2021-04-07 13:45:11 +0000

    completed dockerfiles and docker-compose files and added yml files

--- ssh_into_testVM.sh
+++ ssh_into_testVM.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+#ssh into machine
 ssh -i ~/.ssh/id_rsa ubuntu@<ip-address-of-testVM>
+# remove if already exists
 rm -rf cne-sfia2-brief
+# clone app repo
 git clone https://gitlab.com/qacdevops/cne-sfia2-brief
+
+#install mysql
+sudo apt update -y && sudo apt install mysql-client-core-5.7 -y
+
+#connect to test rds and prepopulate db
+mysql -h (your RDS endpoint) -P 3306 -u admin -p < Create.sql
+
+# run tests
 cd cne-sfia2-brief && pytest && pytest --cov application
