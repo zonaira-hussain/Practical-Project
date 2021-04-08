a8ab916b2cd0251adb82d099d1f9cf84d36b365d
Ubuntu <ubuntu@ip-172-31-29-202.eu-west-1.compute.internal>
2021-04-08 18:05:54 +0000

    something

--- docker-image.sh
+++ docker-image.sh
@@ -3,8 +3,8 @@
 #ssh into machine
 
 
-docker build -t zonaira/backend -f Practical-Project/docker/backend
-docker build -t zonaira/frontend -f Practical-Project/docker/frontend
+sudo docker build -t zonaira/backend -f Practical-Project/docker/backend
+sudo docker build -t zonaira/frontend -f Practical-Project/docker/frontend
 docker-compose up -d
 
 
