1bcb8a4c21966b16393d486845e7b230d0762e67
Ubuntu <ubuntu@ip-172-31-29-202.eu-west-1.compute.internal>
2021-04-08 08:49:59 +0000

    changed test file 2

--- ssh_into_testVM.sh
+++ ssh_into_testVM.sh
@@ -11,7 +11,7 @@ git clone https://gitlab.com/qacdevops/cne-sfia2-brief
 sudo apt update -y && sudo apt install mysql-client-core-5.7 -y
 
 #connect to test rds and prepopulate db
-mysql -h testdb.caumzfitbrs7.eu-west-1.rds.amazonaws.com -P 3306 -u root -p < cne-sfia2-brief/database/Create.sql
+mysql -h testdb.caumzfitbrs7.eu-west-1.rds.amazonaws.com -P 3306 -u root -p passsword < cne-sfia2-brief/database/Create.sql
 
 #set variables
 export TEST_DATABASE_URI=mysql+pymysql://root:password@testdb.caumzfitbrs7.eu-west-1.rds.amazonaws.com:3306/testdb
