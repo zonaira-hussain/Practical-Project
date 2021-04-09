307e757d53afc5d5f327c02bfa2369556c9acb5f
Ubuntu <ubuntu@ip-172-31-29-202.eu-west-1.compute.internal>
2021-04-09 08:01:06 +0000

    new ip address and rds

--- docker-image.sh
+++ docker-image.sh
@@ -3,12 +3,12 @@
 #ssh into jenkins machine
 ssh -i /home/jenkins/.ssh/id_rsa ubuntu@34.244.93.200 << EOF
 
-docker build backend -t zonaira/backend
-docker build frontend -t zonaira/frontend
+sudo docker build backend -t zonaira/backend
+sudo docker build frontend -t zonaira/frontend
 
 #build docker push
-docker push zonaira/frontend:latest
-docker push zonaira/backend:latest
+sudo docker push zonaira/frontend:latest
+sudo docker push zonaira/backend:latest
 
 
 
